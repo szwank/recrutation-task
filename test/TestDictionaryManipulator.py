281c1e9bc2102d663473a77401d552b739f481df
Szwank <lukpankiewicz@gmail.com>
2020-08-03 17:29:50 +0200

    Dodano metode ustawiającą wartość dla podanej ścieżki zagnieżdzonych słowników w klasie DictionaryManipulator.

--- test/TestDictionaryManipulator.py
+++ test/TestDictionaryManipulator.py
@@ -38,6 +38,18 @@ class TestDictionaryManipulator:
 
         assert result == expected_result
 
+    def test_get_element_work_with_ints(self):
+        dict_manipulator = DictionaryManipulator()
+        dictionary = {'file1': 'picture.png',
+                      1: {2: {'file2': 'picture2.jpg', 'file3': 'picture3.png'},
+                                  'file4': 'install.exe'}}
+        path = '\i1/:\i2/:file2'
+        expected_result = 'picture2.jpg'
+
+        result = dict_manipulator.get_from_dictionary(dictionary, path)
+
+        assert result == expected_result
+
 
     def test_get_element_work_with_no_nesting(self):
         dict_manipulator = DictionaryManipulator()
@@ -51,3 +63,36 @@ class TestDictionaryManipulator:
 
         assert result == expected_result
 
+
+    def test_set_element_work_with_no_nesting(self):
+        dict_manipulator = DictionaryManipulator()
+        dictionary = {'file1': 'picture.png',
+                      'folder1': {'folder2': {'file2': 'picture2.jpg', 'file3': 'picture3.png'},
+                                  'file4': 'install.exe'}}
+        path = 'file2'
+        set_value = 'set'
+        expected_result = {'file1': 'picture.png',
+                           'file2': 'set',
+                           'folder1': {'folder2': {'file2': 'picture2.jpg', 'file3': 'picture3.png'},
+                                       'file4': 'install.exe'}}
+
+        dict_manipulator.set_element_in_dictionary(dictionary, path, set_value)
+
+        assert dictionary == expected_result
+
+
+    def test_set_element_work_with_nested_dictionaries(self):
+        dict_manipulator = DictionaryManipulator()
+        dictionary = {'file1': 'picture.png',
+                      'folder1': {'folder2': {'file2': 'picture2.jpg', 'file3': 'picture3.png'},
+                                  'file4': 'install.exe'}}
+        path = 'folder1/:file5'
+        set_value = 'set'
+        expected_result = {'file1': 'picture.png',
+                           'folder1': {'folder2': {'file2': 'picture2.jpg', 'file3': 'picture3.png'},
+                                       'file5': 'set',
+                                       'file4': 'install.exe'}}
+
+        dict_manipulator.set_element_in_dictionary(dictionary, path, set_value)
+
+        assert dictionary == expected_result
