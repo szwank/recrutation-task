0ab6dda9e346703b6e2f487dc3b12a61ef8ae628
Szwank <lukpankiewicz@gmail.com>
2020-08-05 19:26:51 +0200

    dodanie doc stringów do klasy Database

--- database/Database.py
+++ database/Database.py
@@ -14,13 +14,16 @@ from database.tables.TimeZone import TimeZone
 
 
 class Database:
+    """Class for database communication"""
     def __init__(self, filepath: str = 'database.dbo', *args, **kwargs):
         self.__engine = create_engine(f'sqlite:///{filepath}', *args, **kwargs)
         self.__Session = sessionmaker(bind=self.__engine)
 
     def get_session(self):
+        """Returns connection session"""
         return self.__Session()
 
     def create_missing_tables(self):
+        """Create all tables base on classes that inherent from Base class instance."""
         Base.metadata.create_all(self.__engine)
 
