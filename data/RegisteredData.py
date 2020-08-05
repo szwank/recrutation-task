55c12ba4f727eb42e486b8a60ed78ba5d5fb8b43
Szwank <lukpankiewicz@gmail.com>
2020-08-05 20:00:23 +0200

    Zmiana typu zmiennej data w klasie RegistredData

--- data/RegisteredData.py
+++ data/RegisteredData.py
@@ -1,5 +1,10 @@
+import datetime
+
+
 class RegisteredData:
     def __init__(self, date: str, age: int):
-        self.date = date
+        self.date = self.str_to_datetime(date)
         self.age = age
 
+    def str_to_datetime(self, date):
+        return datetime.datetime.strptime(date, '%Y-%m-%dT%H:%M:%S.%f%z')
