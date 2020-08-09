f7d2aa188e8df105f59a7f43f0820ffa85183b82
Szwank <lukpankiewicz@gmail.com>
2020-08-09 13:35:24 +0200

    Poprawienie wyświetlania helpa

--- Options.py
+++ Options.py
@@ -4,44 +4,47 @@ import argparse
 class Options:
     """Parse arguments and store configuration of ArgumentParser class."""
     def __init__(self):
-        self.parser = argparse.ArgumentParser(description="Example script.")
+        self.parser = argparse.ArgumentParser(description="Program for displaying various informations about loaded"
+                                                          "data.")
 
         self.parser.add_argument("--load_data", "-l",
                                  dest="data_path",
-                                 help="Create database if needed and load data from passed path.")
+                                 help="Create database if needed and load data from file in passed path.",
+                                 metavar='PATH_TO_DATA')
 
         self.parser.add_argument("--gender_percentage", "-gp",
                                  dest="get_gender_percentage",
                                  action='store_true',
-                                 help="Returns gender percentage loaded to database.")
+                                 help="Displays gender percentage.")
 
         self.parser.add_argument("--average_age", "-a",
                                  dest="get_age_gender",
                                  choices=['male', 'female', 'both'],
-                                 nargs='?',
-                                 help="Returns average age of genders and overall average age.")
+                                 help="Displays average age of chosen gender.")
 
         self.parser.add_argument("--cities_popularity", "-c",
                                  dest="get_cities_popularity",
-                                 nargs='?',
                                  type=int,
-                                 help="Returns n most popular cities.")
+                                 help="Displays n most popular cities.",
+                                 metavar='N')
 
         self.parser.add_argument("--password_popularity", "-p",
                                  dest="get_password_popularity",
-                                 nargs='?',
                                  type=int,
-                                 help="Returns n most popular passwords.")
+                                 help="Displays n most popular passwords.",
+                                 metavar='N')
 
         self.parser.add_argument("--strongest_password", "-sp",
                                  dest="get_strongest_password",
                                  action='store_true',
-                                 help="Returns strongest password in database.")
+                                 help="Displays strongest password in database.")
 
         self.parser.add_argument("--born_between", "-b",
                                  dest="get_born_between",
                                  nargs=2,
-                                 help="Returns persons born between passed dates. First date should be the earlier one.")
+                                 help="Displays persons born between passed dates. First date should be the earlier"
+                                      "one.",
+                                 metavar=('MIN_DATE', 'MAX_DATE'))
 
         self.arguments = self.parser.parse_args()
 
